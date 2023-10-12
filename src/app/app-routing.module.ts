39aa00e8e373f8436f67dd6cb0de4c7bbc429dcc
seed-ey <123967084+seed-ey@users.noreply.github.com>
2023-10-12 10:21:23 +0100

    implented the Admin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { DealTermsComponent } from './deal-terms/deal-terms.component';
 import { BodyComponent } from './dashboard/body/body.component';
 import { TableComponent } from './table/table.component';
 import { AnalyticsComponent } from './analytics/analytics.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'body', pathMatch: 'full' },
   { path: 'body', component: BodyComponent},
   { path: 'table', component: TableComponent},
   { path: 'analytics', component: AnalyticsComponent},
+  { path: 'admin', component: AdminComponent},
+
   
   
     
